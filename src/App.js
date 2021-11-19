fbeef16418e24dab56a0fc0c7ae0250cb045d20e
Bob <maydaybob2000@gmail.com>
2021-11-19 12:41:33 +0800

    pre-process react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Header, Footer, Sidebar, Card } from 'components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Sidebar />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Box from './Box'
+import Flex from './Flex'
+import Sidebar from './Sidebar'
+import Card from './Card'
+import Button from './Button'
+import Header from './Header'
+import Footer from './Footer'
+
+export { Box, Flex, Sidebar, Card, Button, Footer, Header }
